389d4a8a55f4b7ed0dadf8bc852cc7b3dc70478f
Arthur Purnama <arthur@purnama.de>
2015-11-28 22:48:51 +0100

    [#6] hackmdk3-6: Implement Requirement & Process
    implement requirement & process

---
+++ src/main/resources/static/js/directive/PopOverDirective.js
@@ -0,0 +1,12 @@
+/**
+ * @author Arthur Purnama (arthur@purnama.de)
+ */
+hackMdk3App.directive("hackmdk3PopOver", function () {
+    return {
+        restrict: 'A',
+        link: function (scope, iElement, iAttrs) {
+            $('[data-toggle="tooltip"]').tooltip();
+            $('[data-toggle="popover"]').popover();
+        }
+    }
+});
