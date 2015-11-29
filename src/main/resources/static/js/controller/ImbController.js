0ef954aa4c5b504c4062fce49886ad37d68fe025
Widya Saseno <saseno@gmail.com>
2015-11-29 11:24:54 +0100

    [#4] hackmdk3-4: Implement IMB
    Deleted unused files. correcting template

--- src/main/resources/static/js/controller/ImbController.js
+++ src/main/resources/static/js/controller/ImbController.js
@@ -9,31 +9,42 @@ hackMdk3App.controller('ImbController', ['$scope', '$location',
             {id: 4, name : 'Jawa Timur'}
         ];
 
-        $scope.daftarKabupatenKota = [];
-        $scope.daftarKecamatan = [];
-        $scope.daftarKelurahan = [];
+        $scope.daftarKabupatenKota = [
+            {id: 1, name : '-- Pilih Kabupaten/Kota --'},
+            {id: 2, name : 'Jakarta Barat'},
+            {id: 3, name : 'Jakarta Timur'},
+            {id: 4, name : 'Jakarta Utara'},
+            {id: 5, name : 'Jakarta Selatan'},
+            {id: 6, name : 'Jakarta Pusat'}
+        ];
 
+        $scope.daftarKecamatan = [
+            {id: 1, name : '-- Pilih Kecamatan --'},
+            {id: 2, name : 'Cengkareng'},
+            {id: 3, name : 'Grogol Petamburan'},
+            {id: 4, name : 'Kalideres'},
+            {id: 5, name : 'Kebon Jeruk'},
+            {id: 6, name : 'Kembangan'}
+        ];
+
+        $scope.daftarKelurahan = [
+            {id: 1, name : '-- Pilih Kelurahan --'},
+            {id: 2, name : 'Kedaung'},
+            {id: 3, name : 'Kapuk'},
+            {id: 4, name : 'Cengkareng Barat'},
+            {id: 5, name : 'Cengkareng Timur'},
+            {id: 6, name : 'Rawa Buaya'},
+            {id: 7, name : 'Duri Kosambi'}
+        ];
 
         if ($location.path() === '/imb') {
             $scope.templateUrl = 'templates/imb.info.tpl.html';
-        }
-        else if ($location.path() === '/imb/deskripsi') {
-            $scope.templateUrl = 'templates/imb.deskripsi.tpl.html';
-        }
-        else if ($location.path() === '/imb/lokasi') {
-            $scope.templateUrl = 'templates/imb.lokasi.tpl.html';
-        }
-        else if ($location.path() === '/imb/statistik') {
-            $scope.templateUrl = 'templates/imb.statistik.tpl.html';
-        }
-        else if ($location.path() === '/imb/persyaratan') {
-            $scope.templateUrl = 'templates/imb.persyaratan.tpl.html';
         }
         else if($location.path() === '/imb/proses') {
             $scope.templateUrl = 'templates/imb.proses.tpl.html';
         }
         else if($location.path() === '/imb/review') {
-            $scope.templateUrl = 'templates/addReview.tpl.html';
+            $scope.templateUrl = 'templates/include.review.tpl.html';
         }
         else if($location.path() === '/imb/listDokumen') {
             $scope.templateUrl = 'templates/user.dokument.list.tpl.html';
