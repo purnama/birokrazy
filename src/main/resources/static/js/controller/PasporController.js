9588e9b0ebf34446334e860945fe7cc6beea4112
Andi Sastra <andi.s.wandy@gmail.com>
2015-11-29 16:29:03 +0100

    [#3] hackmdk3-3: Implement E
     paspor and index page

--- src/main/resources/static/js/controller/PasporController.js
+++ src/main/resources/static/js/controller/PasporController.js
@@ -1,45 +1,67 @@
-
 hackMdk3App.controller('PasporController', ['$scope', '$location',
     function ($scope, $location) {
 
         if ($location.path() === '/paspor') {
             $scope.templateUrl = 'templates/paspor.info.tpl.html';
         }
-        else if ($location.path() === '/imb/deskripsi') {
-            $scope.templateUrl = 'templates/paspor.deskripsi.tpl.html';
+        else if ($location.path() === '/paspor/deskripsi') {
+            $scope.templateUrl = 'templates/paspor.info.tpl.html';
         }
-        else if ($location.path() === '/imb/lokasi') {
-            $scope.templateUrl = 'templates/paspor.lokasi.tpl.html';
-        }
-        else if ($location.path() === '/imb/statistik') {
-            $scope.templateUrl = 'templates/paspor.statistik.tpl.html';
-        }
-        else if ($location.path() === '/imb/persyaratan') {
-            $scope.templateUrl = 'templates/paspor.persyaratan.tpl.html';
-        }
-        else if($location.path() === '/imb/proses') {
+        else if ($location.path() === '/paspor/proses') {
             $scope.templateUrl = 'templates/paspor.proses.tpl.html';
         }
-        else if($location.path() === '/imb/lapor') {
+        else if ($location.path() === '/paspor/lapor') {
             $scope.templateUrl = 'templates/paspor.lapor.tpl.html';
-        }
-        else {
-            $scope.templateUrl = 'templates/paspor.info.tpl.html';
         }
 
         $scope.isActive = function (viewLocation) {
             return viewLocation === $location.path();
         };
 
-        $(".rating").rating({
-            readonly: true,
-            showClear: false
-        });
+        $scope.listOfOptionsDomisili = [
+            {id: 0, name: '--pilih--'},
+            {id: 1, name: 'Indonesia'},
+            {id: 2, name: 'Luar negeri'}
+        ];
 
-        $scope.testFunction = function(){
-            alert('testing....');
-            $scope.testValue = 'testing value...';
-        };
+        $scope.listOfOptionsPeruntukan = [
+            {id: 0, name: '--pilih--'},
+            {id: 1, name: 'Dewasa'},
+            {id: 2, name: 'Anak-anak'}
+        ];
 
+        $scope.listOfOptionsProses = [
+            {id: 0, name: '--pilih--'},
+            {id: 1, name: 'Manual'},
+            {id: 2, name: 'Elektronik'}
+        ];
+
+        $scope.isShowPeruntukan = false;
+        $scope.isShowProses = false;
+        $scope.isShowSyaratDanProses = false;
+
+        $scope.pasporDomisiliPaspor = function (domisiliPaspor) {
+            if (domisiliPaspor.id !== 0) {
+                $scope.isShowPeruntukan = true;
+            } else {
+                $scope.isShowPeruntukan = false;
+            }
+        }
+
+        $scope.pasporPeruntukanPaspor = function (peruntukanPaspor) {
+            if (peruntukanPaspor.id !== 0) {
+                $scope.isShowProses = true;
+            } else {
+                $scope.isShowProses = false;
+            }
+        }
+
+        $scope.pasporProsesPaspor = function (prosesPaspor) {
+            if (prosesPaspor.id !== 0) {
+                $scope.isShowSyaratDanProses = true;
+            } else {
+                $scope.isShowSyaratDanProses = false;
+            }
+        }
 
     }]);
