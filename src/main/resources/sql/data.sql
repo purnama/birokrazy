9cf2014b753954a310a55d41aa6f1291d72dc9b5
Arthur Purnama <arthur@purnama.de>
2015-12-02 22:46:49 +0100

    [#10] hackmdk3-10: Implement tutorial or about page
    implement landing page with information.
    tutorial and introduction will coming with video.
    consider this task done.

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO highlight (image, description) VALUES ('fitur-informasi.png', 'Description one donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.');
-INSERT INTO highlight (image, description) VALUES ('fitur-tracking.png', 'Description two donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.');
-INSERT INTO highlight (image, description) VALUES ('fitur-statistik.png', 'Description three donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.');
+INSERT INTO highlight (image, description) VALUES ('fitur-informasi.png', 'Birokrazy memberikan informasi terlengkap tentang apa saja yang harus dipersiapkan, prosedur pengajuan dan tanggung jawab instansi terkait.');
+INSERT INTO highlight (image, description) VALUES ('fitur-tracking.png', 'Birokrazy memungkinkan kita untuk memantau sendiri proses pengerjaan dokumen setiap saat.');
+INSERT INTO highlight (image, description) VALUES ('fitur-statistik.png', 'Birokrazy memberi kita kesempatan untuk menilai proses pengurusan dokumen, berbagi pengalaman dan informasi terbaru.');
 
 INSERT INTO user (username, password, name, email) VALUES ('admin', 'admin', 'Bimantara Adipramana', 'bimantara.adipramana@momoku.net');
 INSERT INTO user (username, password, name, email) VALUES ('user', 'user', 'Pramudya Cokroatmojo', 'pramudya.cokroatmojo@momoku.net');
