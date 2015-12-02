aad8e468c5ad52981cc35fd13c27cecf2ce793eb
Widya Saseno <saseno@gmail.com>
2015-12-02 20:30:13 +0100

    [#4] hackmdk3-4: Implement IMB
    Process Checkbox in Iyin Usaha, sequential

--- src/main/resources/static/js/controller/IzinUsahaController.js
+++ src/main/resources/static/js/controller/IzinUsahaController.js
@@ -65,6 +65,45 @@ hackMdk3App.controller('IzinUsahaController', ['$scope', '$location', '$rootScop
             }else {
                 $scope.isShowProses = false;
             }
+
         };
+
+        $scope.Math = window.Math;
+
+        $scope.isActiveStep2 = true;
+        $scope.isActiveStep3 = true;
+        $scope.isActiveStep4 = true;
+        $scope.isActiveStep5 = true;
+        $scope.isActiveStep6 = true;
+
+        $scope.processCheckBox = function (confirmed) {
+            if (confirmed) {
+                $scope.counter++;
+            } else {
+                $scope.counter--;
+            }
+
+            $scope.currentBar = Math.floor(($scope.counter / $scope.maxbar ) * 100);
+            $scope.currentWidth = 'width: ' + $scope.currentBar + '%';
+
+            if ($scope.counter === 1) {
+                $scope.isActiveStep2 = false;
+
+            } else if ($scope.counter === 2) {
+                $scope.isActiveStep3 = false;
+
+            } else if ($scope.counter === 3) {
+                $scope.isActiveStep4 = false;
+
+            } else if ($scope.counter === 4) {
+                $scope.isActiveStep5 = false;
+
+            } else if ($scope.counter === 5) {
+                $scope.isActiveStep6 = false;
+
+            } else if ($scope.counter === 6) {
+                $scope.isActiveStep7 = false;
+            }
+        }
 
     }]);
