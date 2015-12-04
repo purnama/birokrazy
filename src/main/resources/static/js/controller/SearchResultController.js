fa7609e376b64e8131bfce5918c569d3b2ad05cf
Arthur Purnama <arthur@purnama.de>
2015-12-04 13:44:42 +0100

    [#20] hackmdk3-20: Implement target for search autocomplete
    implemented

--- src/main/resources/static/js/controller/SearchResultController.js
+++ src/main/resources/static/js/controller/SearchResultController.js
@@ -15,6 +15,18 @@ hackMdk3App.controller('SearchResultController', ['$scope', '$location',
             return viewLocation === $location.path();
         };
 
-        $scope.finds = ['Izin Usaha Toko', 'Surat Izin Usaha Perdagangan', 'Izin Mendirikan bangunan', 'Kartu Tanda Penduduk',
-        'Kartu Tanda Penduduk Elektronik', 'KTP', 'E-KTP', 'Paspor'];
+        $scope.finds = [
+            {name: 'Izin Usaha Ritel', url: "/izin-usaha"},
+            {name: 'Surat Izin Usaha Perdagangan', url: "#"},
+            {name: 'Izin Mendirikan bangunan', url: "/imb"},
+            {name: 'Kartu Tanda Penduduk', url: "#"},
+            {name: 'Kartu Tanda Penduduk Elektronik', url: "/e-ktp"},
+            {name: 'KTP', url: "#"},
+            {name: 'E-KTP', url: "/e-ktp"},
+            {name: 'Paspor', url: "/paspor"}
+        ];
+
+        $scope.selectAction = function ($item, $model, $label) {
+            $location.path($model.url);
+        }
     }]);
