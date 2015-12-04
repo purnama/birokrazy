f8c2c2482d6ade9c3cf3ad6e04787a9ba311fcf0
Widya Saseno <saseno@gmail.com>
2015-12-04 15:45:12 +0100

    [#4] hackmdk3-4: Updated Navigator Bar in Main page.
    Selected Page now will have class "active".

--- src/main/resources/static/js/controller/HeaderController.js
+++ src/main/resources/static/js/controller/HeaderController.js
@@ -4,7 +4,16 @@
 hackMdk3App.controller('HeaderController', ['$scope', '$location', function ($scope, $location) {
     $scope.isActive = function (viewLocation) {
         /* return viewLocation === $location.path(); */
-        console.log('if : ' + viewLocation + ' same : ' + $location.path());
-        return $location.path().indexOf(viewLocation) === 0;
+        var isDebug = false;
+
+        if(isDebug) {
+            console.log('if : ' + viewLocation + ' same : ' + $location.path());
+        }
+
+        if(viewLocation.length > 1) {
+            return $location.path().indexOf(viewLocation) === 0;
+        }else {
+            return viewLocation === $location.path();
+        }
     };
 }]);
