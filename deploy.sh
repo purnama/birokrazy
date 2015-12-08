439ce81aded35c40dc741cff785a6f765c81a436
Arthur Purnama <arthur@purnama.de>
2015-12-08 23:20:42 +0100

    [#32] hackmdk3-32: Prepare Grunt Build
    create deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ if [ ! -h $mda ]; then
   ln -s /home/ubuntu/hackmdk3/target/hackmdk3-0.0.1-SNAPSHOT.war $mda
 fi
 $mda stop
-git pull
-mvn clean package
+git -C /home/ubuntu/hackmdk3 pull
+mvn -f /home/ubuntu/hackmdk3 clean package
 $mda start
