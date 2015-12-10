a08bf6a2edbd6ed91dd5662106f8cb66404f3f97
Arthur Purnama <arthur@purnama.de>
2015-12-10 16:58:00 +0100

    [#32] hackmdk3-32: Prepare Grunt Build
    set abstract controller

--- src/main/java/id/hackathonmerdeka/hackmdk3/controller/ProtectedController.java
+++ src/main/java/id/hackathonmerdeka/hackmdk3/controller/ProtectedController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "api/v1/protected")
-public class ProtectedController {
+abstract public class ProtectedController {
 }
--- src/main/java/id/hackathonmerdeka/hackmdk3/controller/PublicController.java
+++ src/main/java/id/hackathonmerdeka/hackmdk3/controller/PublicController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "api/v1/public")
-public class PublicController {
+abstract public class PublicController {
 }
