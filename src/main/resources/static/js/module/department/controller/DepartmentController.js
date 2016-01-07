ceece671ecb75aaea4976d4ccb66abe900a8f56e
Arthur Purnama <arthur@purnama.de>
2016-01-07 11:09:36 +0100

    [#34] hackmdk3-34: Implement CRUD for Department
    implementing crud for departement
    implementing departement site

---
+++ src/main/resources/static/js/module/department/controller/DepartmentController.js
@@ -0,0 +1,53 @@
+/**
+ * @author Arthur Purnama (arthur@purnama.de)
+ */
+"use strict";
+departmentModule.controller('departmentModule.DepartmentController', ['$scope', '$location', '$constant', '$routeParams', 'HighchartService',
+    'DurationModalService', 'DepartmentService',
+    function ($scope, $location, $constant, $routeParams, highchartService, durationModalService, departmentService) {
+
+        $scope.waitingTime = highchartService.waitingTime;
+        $scope.department = {};
+        $scope.modulePath = $constant.module.department.path;
+        departmentService.findById($routeParams.name).then(function (data) {
+            data.geoLocation = {
+                latitude: data.latitude,
+                longitude: data.longitude
+            };
+            $scope.department = data;
+            $scope.departmentPath = $routeParams.name;
+            if ($location.path() === '/' + $constant.module.department.path + '/' + $scope.departmentPath + '/review') {
+                departmentService.findAllReviewById($routeParams.name).then(function (data) {
+                    $scope.department.reviewList = data;
+                    $scope.templateUrl = 'templates/include.review.tpl.html';
+                });
+            } else if ($location.path() === '/' + $constant.module.department.path + '/' + $scope.departmentPath + '/trend') {
+                $scope.templateUrl = 'templates/include.trend.tpl.html';
+            } else if ($location.path() === '/' + $constant.module.department.path + '/' + $scope.departmentPath + '/process') {
+                $scope.templateUrl = 'templates/department.process.tpl.html';
+            } else if ($location.path() === '/' + $constant.module.department.path + '/' + $scope.departmentPath) {
+                $scope.templateUrl = $constant.module.department.templates + 'department.info.tpl.html';
+            }
+        });
+
+
+        $scope.duration = {
+            modal: false,
+            checked: false,
+            units: $constant.duration.units,
+            value: 2,
+            unit: $constant.duration.minggu
+        };
+
+
+        $scope.modalOpen = function () {
+            durationModalService.open($scope);
+        };
+
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+
+    }
+])
+;
